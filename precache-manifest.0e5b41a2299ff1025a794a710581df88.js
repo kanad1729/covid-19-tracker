ba8478416af93fa0f8b3e1b5fe81246d34c9ffbf
kanad1729 <rahulpagar1510@gmail.com>
2020-04-21 14:52:48 +0530

    Updates

---
+++ precache-manifest.0e5b41a2299ff1025a794a710581df88.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b150f45ab30989833d88b0fc061c2f9f",
+    "url": "/covid-19-tracker/index.html"
+  },
+  {
+    "revision": "72e9a32be12bbe387528",
+    "url": "/covid-19-tracker/static/css/main.70caba3c.chunk.css"
+  },
+  {
+    "revision": "6589430b7007c67877eb",
+    "url": "/covid-19-tracker/static/js/2.0958160a.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid-19-tracker/static/js/2.0958160a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72e9a32be12bbe387528",
+    "url": "/covid-19-tracker/static/js/main.a8e7ae57.chunk.js"
+  },
+  {
+    "revision": "3d1c3a839b5135420af2",
+    "url": "/covid-19-tracker/static/js/runtime-main.94eb6b52.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid-19-tracker/static/media/img19.d7265326.png"
+  }
+]);
